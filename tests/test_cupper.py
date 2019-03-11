ee6ff18c24013301f656e89a0fef91e8a4d8bf42
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-03-11 17:04:04 +0100

    cleanup

--- tests/test_cupper.py
+++ tests/test_cupper.py
@@ -1,23 +1,5 @@
-import pytest
-
-from cupper import example
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/thomasjahoda/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    example.example_fn()
-
-# def test_command_line_interface():
+def test_command_line_interface():
+    pass
 #     runner = CliRunner()
 #     result = runner.invoke(cli.main)
 #     assert result.exit_code == 0
